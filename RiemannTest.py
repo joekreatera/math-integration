f7a506385006587a6f767085436f135cb9304e4b
Joe Mondragon <jose.c.mondragon@gmail.com>
2019-11-05 12:50:08 -0600

    Finished Riemann
    riemann integration done

--- RiemannTest.py
+++ RiemannTest.py
@@ -3,13 +3,41 @@
 def linear(x):
     return x
 
+def quadratic(x):
+    return x*x
+
+
 def riemannIntegration(fn,a,b,n):
 
     integrationTotal = 0
-    
-    return integrationTotal
+    base = (b-a)/n
+    halfBase = base/2
+    for i in range(0,n):
+        integrationTotal = integrationTotal + fn(i*base+halfBase)
+    return (integrationTotal*base)
 
 
 print("Riemann test!")
-totalIntegration= riemannIntegration(linear , 0, 3, 1)
-print("Riemann result " , totalIntegration )
+totalIntegration= riemannIntegration(quadratic , 0, 3, 1)
+print("Riemann result 1 " , totalIntegration )
+totalIntegration= riemannIntegration(quadratic , 0, 3, 2)
+print("Riemann result 2" , totalIntegration )
+totalIntegration= riemannIntegration(quadratic , 0, 3, 3)
+print("Riemann result 3" , totalIntegration )
+totalIntegration= riemannIntegration(quadratic , 0, 3, 4)
+print("Riemann result 4" , totalIntegration )
+totalIntegration= riemannIntegration(quadratic , 0, 3, 5)
+print("Riemann result 5" , totalIntegration )
+totalIntegration= riemannIntegration(quadratic , 0, 3, 100)
+print("Riemann result 100" , totalIntegration )
+
+
+
+
+
+
+
+
+
+
+
