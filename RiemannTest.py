61125f785aa348d481f1698c6607bd3808373ff8
Joe Mondragon <jose.c.mondragon@gmail.com>
2019-11-05 12:26:19 -0600

    Base architecture
    Init functions

--- RiemannTest.py
+++ RiemannTest.py
@@ -1,3 +1,15 @@
 #riemann rest for integration
 
+def linear(x):
+    return x
+
+def riemannIntegration(fn,a,b,n):
+
+    integrationTotal = 0
+    
+    return integrationTotal
+
+
 print("Riemann test!")
+totalIntegration= riemannIntegration(linear , 0, 3, 1)
+print("Riemann result " , totalIntegration )
